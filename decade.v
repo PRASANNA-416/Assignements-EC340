a22879f83a8895498dd0a952bb954b5bb818d315
Prasanna <76594416+PRASANNA-416@users.noreply.github.com>
2022-02-14 23:39:33 +0530

    Update decade.v

--- decade.v
+++ decade.v
@@ -10,12 +10,18 @@ module decade(load,reset,data_in,counter_on,counter_up,clock,TC,count);
         count <= 0;
   else if (counter_on == 1 & counter_up == 1)
       if(count == 4'b1001)
+        begin
         TC <= 1;
+        count <= 0;
+        end
       else
         count <= count + 1;
   else if (counter_on == 1 & counter_up == 0)
     if(count == 0)
+      begin
         TC <= 1;
+        count <= 4'b1001;
+      end
       else
         count <= count - 1;
   
