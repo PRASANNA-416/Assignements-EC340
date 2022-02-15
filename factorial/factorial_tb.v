082806413b472832dcb62a710bc0b93c73366338
Prasanna <76594416+PRASANNA-416@users.noreply.github.com>
2022-02-15 22:50:46 +0530

    Update factorial_tb.v

--- factorial/factorial_tb.v
+++ factorial/factorial_tb.v
@@ -2,21 +2,19 @@
 
 parameter t_PERIOD =100;
 reg clk, reset_n, start;
-  reg [3:0] data1;
+reg [3:0] data1;
 wire done;
-  wire [15:0] fact;
+wire [15:0] fact;
 
   factorial  uut (clk,reset_n,start,data1,done,fact);
 //clock generation
 
 initial
  begin
-   clk =0; reset_n =0; start =0; data1 = 4'b0000; 
-    #150 reset_n = 1; data1 = 4'b0110;
-    #200 reset_n = 1;data1 = 4'b1100;
-    #250 reset_n = 1;data1 = 4'b0110;
-    #300 start = 1;
-    #500 start = 0;
+   clk =0; reset_n =1; start =0; data1 = 4'b0000; 
+    #150 reset_n = 0;start = 1; data1 = 4'b0110; 
+
+
   end
 
 initial
@@ -25,5 +23,6 @@ initial
 always @(done) 
    if (done ==1)
        $finish;
+    
     
 endmodule
