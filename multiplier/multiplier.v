b17e24576b7d51b89ff51df9b0019ac52d27f73c
Prasanna <76594416+PRASANNA-416@users.noreply.github.com>
2022-02-15 23:58:15 +0530

    Update multiplier.v

--- multiplier/multiplier.v
+++ multiplier/multiplier.v
@@ -1,14 +1,14 @@
-module multiplier(input clock, reset_n, start,
-input [3:0] DataA, DataB,
-output reg [7:0] product, 
+	module multiplier(input clock, reset_n, start,
+input [7:0] DataA, DataB,
+output reg [15:0] product, 
 output reg done);
 
 reg [1:0] Comp_d ,Comp_q;
 
 parameter [1:0] S0=2'b00, S1=2'b01, S2=2'b10;
 reg [1:0] cp_fsm_d, cp_fsm_q;
-reg [3:0] N_zeros, B_d, B_q;
-reg [7:0] A_d, A_q,pdt_d, pdt_q;
+reg [7:0] N_zeros, B_d, B_q;
+reg [15:0] A_d, A_q,pdt_d, pdt_q;
 
 always @(posedge clock or posedge reset_n)
 begin
@@ -39,11 +39,11 @@ begin
     pdt_d = pdt_q; A_d = A_q; B_d = B_q;Comp_d = Comp_q;
     case (cp_fsm_q)
     S0: begin
-        pdt_d = 8'b0;
-        if (DataA[3] == 1'b1)
+        pdt_d = 16'b0;
+        if (DataA[7] == 1'b1)
         begin
-            A_d[3:0] = DataA;
-            A_d[7:4] = 4'b1111;
+            A_d[7:0] = DataA;
+            A_d[15:8] = 8'b11111111;
         end
         else 
             A_d = DataA;
