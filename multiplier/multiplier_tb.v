701edb4976cbf475e38d5ef7d484875e7fef4226
Prasanna <76594416+PRASANNA-416@users.noreply.github.com>
2022-02-16 00:14:29 +0530

    Perfectly working with tb

--- multiplier/multiplier_tb.v
+++ multiplier/multiplier_tb.v
@@ -3,29 +3,31 @@ module multiplier_tb;
 
 parameter t_PERIOD =100;
 reg clk, reset_n, start;
-reg [7:0] data1, data2;
+reg [7:0] data1;
+reg [7:0] data2;
 wire done;
 wire [15:0] product;
 
-  multiplier  uut (clk,reset_n,start,data1,data2,product,done);
+multiplier  uut (clk,reset_n,start,data1,data2,product,done);
 //clock generation
 
 initial
  begin
-    clk =0; reset_n =0; start =0; data1 = {8{1'b0}}; data2 = {8{1'b0}}; 
-    #150 reset_n = 1; data1 = 8'b00011010; data2 = 8'b00101101;
-    #200 reset_n = 1; data1 = 8'b01100100; data2 = 8'b10110011;
-    #250 reset_n = 1; data1 = 8'b11000111; data2 = 8'b00111001;
-    #300 reset_n = 1; data1 = 8'b11001100; data2 = 8'b10001110;
-    #350 start = 1;
-    #550 start = 0;
+   clk =0; reset_n =0; start =0; data1 = 8'b00000000;data2 = 8'b00000000; 
+    #150 reset_n = 1;start = 1;data1 = 8'b00011010; data2 = 8'b00101101;  
+    #2000 start = 0;reset_n = 0;
+    #100 reset_n = 1;start = 1;data1 = 8'b01100100; data2 = 8'b10110011;
+    #2000 start = 0;reset_n = 0;
+    #100 reset_n = 1;start = 1;data1 = 8'b11000111; data2 = 8'b00111001;
+    #2000 start = 0;reset_n = 0;
+    #100 reset_n = 1;start = 1;data1 = 8'b11000111; data2 = 8'b00111001;
+    
+
+
   end
 
 initial
     forever #(t_PERIOD/2) clk <= ~clk;
 
-always @(done) 
-   if (done ==1)
-       $finish;
-    
 endmodule
+
