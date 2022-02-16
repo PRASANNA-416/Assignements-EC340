1025a3f68393ff05a2a08e10bfcfbb2b0dc726af
Prasanna <76594416+PRASANNA-416@users.noreply.github.com>
2022-02-16 09:14:17 +0530

    Update multiplier_tb.v

--- multiplier/multiplier_tb.v
+++ multiplier/multiplier_tb.v
@@ -13,14 +13,14 @@ multiplier  uut (clk,reset_n,start,data1,data2,product,done);
 
 initial
  begin
-   clk =0; reset_n =0; start =0; data1 = 8'b00000000;data2 = 8'b00000000; 
-    #150 reset_n = 1;start = 1;data1 = 8'b00011010; data2 = 8'b00101101;  
-    #2000 start = 0;reset_n = 0;
-    #100 reset_n = 1;start = 1;data1 = 8'b01100100; data2 = 8'b10110011;
-    #2000 start = 0;reset_n = 0;
-    #100 reset_n = 1;start = 1;data1 = 8'b11000111; data2 = 8'b00111001;
-    #2000 start = 0;reset_n = 0;
-    #100 reset_n = 1;start = 1;data1 = 8'b11000111; data2 = 8'b00111001;
+   clk =0; reset_n =1; start =0; data1 = 8'b00000000;data2 = 8'b00000000; 
+    #150 reset_n = 0;start = 1;data1 = 8'b00011010; data2 = 8'b00101101;  
+    #2000 start = 0;reset_n = 1;
+    #100 reset_n = 0;start = 1;data1 = 8'b01100100; data2 = 8'b10110011;
+    #2000 start = 0;reset_n = 1;
+    #100 reset_n = 0;start = 1;data1 = 8'b11000111; data2 = 8'b00111001;
+    #2000 start = 0;reset_n = 1;
+    #100 reset_n = 0;start = 1;data1 = 8'b11000111; data2 = 8'b00111001;
     
 
 
