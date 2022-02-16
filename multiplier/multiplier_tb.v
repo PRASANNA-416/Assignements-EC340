2d3a1d9d193ea9355043bd0bb26af178dacf05b7
Prasanna <76594416+PRASANNA-416@users.noreply.github.com>
2022-02-16 20:27:32 +0530

    Update multiplier_tb.v

--- multiplier/multiplier_tb.v
+++ multiplier/multiplier_tb.v
@@ -1,5 +1,4 @@
-	
-module multiplier_tb;
+	module multiplier_tb;
 
 parameter t_PERIOD =100;
 reg clk, reset_n, start;
@@ -15,12 +14,12 @@ initial
  begin
    clk =0; reset_n =1; start =0; data1 = 8'b00000000;data2 = 8'b00000000; 
     #150 reset_n = 0;start = 1;data1 = 8'b00011010; data2 = 8'b00101101;  
-    #2000 start = 0;reset_n = 1;
+    #1500 start = 0;reset_n = 1;
     #100 reset_n = 0;start = 1;data1 = 8'b01100100; data2 = 8'b10110011;
-    #2000 start = 0;reset_n = 1;
+    #1500 start = 0;reset_n = 1;
     #100 reset_n = 0;start = 1;data1 = 8'b11000111; data2 = 8'b00110001;
-    #2000 start = 0;reset_n = 1;
-    #100 reset_n = 0;start = 1;data1 = 8'b11000111; data2 = 8'b00111001;
+    #1500 start = 0;reset_n = 1;
+    #100 reset_n = 0;start = 1;data1 = 8'b10100111; data2 = 8'b10111001;
     
 
 
@@ -30,4 +29,3 @@ initial
     forever #(t_PERIOD/2) clk <= ~clk;
 
 endmodule
-
