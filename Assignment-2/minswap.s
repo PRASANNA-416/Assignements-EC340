0b2a8c660f00f2b0265ebda6745f845460b3b69a
Prasanna <76594416+PRASANNA-416@users.noreply.github.com>
2022-02-25 19:57:44 +0530

    initial

---
+++ Assignment-2/minswap.s
@@ -0,0 +1,13 @@
+# to find minimum value in array and swap with last item 
+
+        .data
+        
+ array: .word 7 12 5 10 8 9 4 2 15 13
+ 
+ 
+        .text
+        .globl main
+        
+main:
+        ##
+        
