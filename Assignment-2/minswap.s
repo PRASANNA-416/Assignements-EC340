46051b72533c74f96843e9c0aa6c2049161639b3
Prasanna <76594416+PRASANNA-416@users.noreply.github.com>
2022-02-26 22:04:10 +0530

    Update minswap.s

--- Assignment-2/minswap.s
+++ Assignment-2/minswap.s
@@ -10,37 +10,38 @@
         
 main:   
 
-        la      $t0, array
+        la      $s0, array
         li      $t1, 1
-        lw      $s2, 0($t0)
+        lw      $s2, 0($s0)
 loop:   slti    $t3, $t1, 10         # if i == 10 goto done
         beq     $t3, $zero, done
 
-        lw      $t4, 0($t0)
-        slt     $t7, $t4, $s2
-        bne     $t7, $zero,Nmin
+        lw      $t4, 0($s0)
+        slt     $t6, $t4, $s2
+        bne     $t6, $zero,Nmin
         addi    $t1, $t1, 1         # i++
-        addu    $t0, $t0, 4
+        addu    $s0, $s0, 4
         j       loop
 
 Nmin:   
         move    $s2,$t4
         add     $s3,$t1,$zero
-        #lw     $s3,0($t0)
+        #lw     $s3,0($s0)
         j       loop
 
 done:   
-        lw      $t5, 0($t0)
-        sw      $s2, 0($t0)
+        lw      $t5, 0($s0)
+        sw      $s2, 0($s0)
         nor     $s3, $s3, $zero
         addi    $s3,$s3,1
         addi    $s3, $s3,10
         sll     $s3 , $s3 , 2
         nor     $s3,  $s3, $zero
         addi    $s3, $s3,1
-        add    $t0 ,$t0, $s3
-        sw      $t5, 0($t0)
+        add     $s0 ,$s0, $s3
+        sw      $t5, 0($s0)
 
+        
         li      $v0, 1              # print min element
         move    $a0, $s2
         syscall
