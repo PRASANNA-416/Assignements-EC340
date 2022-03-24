f6dd646d142e3cdb87c09e530a80f267ec8d8844
Prasanna <76594416+PRASANNA-416@users.noreply.github.com>
2022-03-24 23:06:27 +0530

    added new control signal Notbranch(bne)

--- Assignment 3/control.v
+++ Assignment 3/control.v
@@ -1,4 +1,4 @@
-module control(opcode, RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, AluOP);
+module control(opcode, RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch,NotBranch, AluOP);
 
 input [5:0] opcode;
 
@@ -9,20 +9,21 @@ output reg RegWrite;
 output reg MemRead;
 output reg MemWrite;
 output reg Branch;
+output reg NotBranch;
 
 output reg [1:0] AluOP;
 
 always @(opcode) begin
 	case (opcode)
-		6'b000000:{RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, AluOP}=9'b100100_0_10; //r
-		6'b100011:{RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, AluOP}=9'b011110_0_00; //lw
-		6'b101011:{RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, AluOP}=9'bx1x001_0_00; //sw
-		6'b000100:{RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, AluOP}=9'bx0x000_1_01; //beq
-		6'b001000:{RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, AluOP}=9'b010100_0_00; //addi
-		6'b000101:{RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, AluOP}=9'bx0x000_1_01; //bne
-		6'b000010:{RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, AluOP}=9'b      _1_01; //j
+		6'b000000:{RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch,NotBranch, AluOP}=10'b100100_00_10; //r
+		6'b100011:{RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch,NotBranch, AluOP}=10'b011110_00_00; //lw
+		6'b101011:{RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch,NotBranch, AluOP}=10'bx1x001_00_00; //sw
+		6'b000100:{RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch,NotBranch, AluOP}=10'bx0x000_10_01; //beq
+		6'b001000:{RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch,NotBranch, AluOP}=10'b010100_00_00; //addi
+		6'b000101:{RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch,NotBranch, AluOP}=10'bx0x000_01_01; //bne
+		6'b000010:{RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch,NotBranch, AluOP}=10'b      _00_01; //j
 		default:
-	{RegDst,ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch,AluOP}=9'bxxx_xxx_x_xx;
+		{RegDst,ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch,NotBranch,AluOP}=10'bxxx_xxx_xx_xx;
 	endcase
 end
 
