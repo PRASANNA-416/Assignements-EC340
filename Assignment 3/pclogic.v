2525f9d2cc8acd44e52486dc29c1c2b68239ff6e
Prasanna <76594416+PRASANNA-416@users.noreply.github.com>
2022-03-25 18:07:35 +0530

    implemented jump

--- Assignment 3/pclogic.v
+++ Assignment 3/pclogic.v
@@ -1,23 +1,28 @@
-module pclogic(clk, reset, ain, aout, pcsel);
+module pclogic(clk, reset, ain, aout, pcsel,jump,Instruction);
 
 input reset;
 input clk;
 input [31:0] ain;
 //pecsel = branch & zero
 input pcsel;
+intput [25:0]Instruction;
 
 output reg [31:0] aout;
 
-always @(posedge clk ) begin
+always @(posedge clk) begin
 	if (reset==1)
 		aout<=32'b0;
 	else
-		if (pcsel==0) begin
+		if (pcsel==0 and jump==0) begin
 			aout<=aout+1;
 		end
-		if (pcsel==1) begin
+		if (pcsel==1 and jump==0) begin
 			aout<=ain+aout+1; //branch
-	end
+		end
+		if(jump==1) begin
+			Instruction<= Instruction<<2;
+			aout<= {aout[31:28],Instruction};
+		end
 end
 
 
